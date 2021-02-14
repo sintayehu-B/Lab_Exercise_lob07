5628472477ce83edf4ad7c15c128c487a4cfcf02
sintayehu-B <61502710+sintayehu-B@users.noreply.github.com>
2021-02-15 01:25:15 +0300

    added gallery

---
+++ Finished/asset/js/gallery.js
@@ -0,0 +1,13 @@
+// $('.carousel').carousel({
+//     interval: 5000
+// });
+// $('.carousel').on('slid.bs.carousel', function (e) {
+//     ChangeThePicture();
+// });
+// async function ChangeThePicture() {
+
+//     let response = await fetch('https://picsum.photos/1280/720');
+//     let data = await response.url;
+//     let activeImage = document.querySelector(".carousel-item.active").querySelector("img");
+//     activeImage.src = data;
+// }
